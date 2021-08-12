9bdb08807bdc4d423ba703af84cbb0063c978267
052hide <hidetoshi1990@gmail.com>
2021-08-13 00:30:46 +0900

    input使用サンプル作成

--- src/containers/sample-container/sample-container.tsx
+++ src/containers/sample-container/sample-container.tsx
@@ -1,4 +1,4 @@
-import { Component, Method, h } from '@stencil/core'
+import { Component, State, Method, h } from '@stencil/core'
 
 @Component({
   tag: 'sample-container',
@@ -6,6 +6,8 @@ import { Component, Method, h } from '@stencil/core'
   shadow: true,
 })
 export class SampleContainer {
+  @State() value: string = ''
+
   /**
    * click event
    */
@@ -14,12 +16,22 @@ export class SampleContainer {
     console.log('handleClicked')
   }
 
+  /**
+   * input event
+   */
+  @Method()
+  async handleInput(event: CustomEvent<string>) {
+    this.value = event.detail
+  }
+
   render() {
     return (
       <div>
         <wc-button html-type="button" disabled={false} loading={true} theme="primary" size="base" block={true} onClicked={this.handleClicked}>
           Button
         </wc-button>
+        <wc-input name="input" placeholder="placeholder" disabled={true} value={this.value} onChangeValue={event => this.handleInput(event)} />
+        {this.value}
       </div>
     )
   }
